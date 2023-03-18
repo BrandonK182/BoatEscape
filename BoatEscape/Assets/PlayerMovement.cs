5a3bb9a6ea97193e5fd50a66e3e07518998dabbf
Brandon Kieng <brandonkieng182@gmail.com>
2023-03-18 13:13:59 -0700

    player movement

--- BoatEscape/Assets/PlayerMovement.cs
+++ BoatEscape/Assets/PlayerMovement.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody2D player;
+
     // Start is called before the first frame update
-    void Start()
+    private void Start()
     {
         Debug.Log("Hello World!");
+        player = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        if (Input.GetKeyDown("space"))
+        float dirX = Input.GetAxisRaw("Horizontal");
+        player.velocity = new Vector2(dirX * 7f, player.velocity.y);
+
+        if (Input.GetButtonDown("Jump"))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector3(0,7,0);
+            player.velocity = new Vector3(player.velocity.x,7,0);
         }
     }
 }
