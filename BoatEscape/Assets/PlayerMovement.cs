72e3fb662e9ab967b0134242db64a782b60e9f5c
Brandon Kieng <brandonkieng182@gmail.com>
2023-03-18 14:05:19 -0700

    added animations

--- BoatEscape/Assets/PlayerMovement.cs
+++ BoatEscape/Assets/PlayerMovement.cs
@@ -5,23 +5,49 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     private Rigidbody2D player;
+    private Animator anim;
+    private SpriteRenderer sprite;
+
+    private float dirX = 0.0f;
 
     // Start is called before the first frame update
     private void Start()
     {
         Debug.Log("Hello World!");
         player = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        sprite = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     private void Update()
     {
-        float dirX = Input.GetAxisRaw("Horizontal");
+        dirX = Input.GetAxis("Horizontal");
         player.velocity = new Vector2(dirX * 7f, player.velocity.y);
 
         if (Input.GetButtonDown("Jump"))
         {
             player.velocity = new Vector3(player.velocity.x,7,0);
+        }
+
+        UpdateAnimation();
+    }
+
+    private void UpdateAnimation()
+    {
+        if (dirX > 0.0f)
+        {
+            anim.SetBool("is_running", true);
+            sprite.flipX = false;
+        }
+        else if (dirX < 0.0f)
+        {
+            anim.SetBool("is_running", true);
+            sprite.flipX = true;
+        }
+        else
+        {
+            anim.SetBool("is_running", false);
         }
     }
 }
